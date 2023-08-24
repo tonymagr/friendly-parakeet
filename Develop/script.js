e94eafa2a36668aa16933a60605e34db87414d73
Tony Magrady <tonymagrady@gmail.com>
2023-08-24 16:32:06 -0700

    mod 3 challenge- gather password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,57 @@
 // Assignment code here
+let validUserInput = false;
 
+function generatePassword() {
+  let genPassword = "dummy@K9";
+  let pwdLenNum;
+  let responseCode;
+
+  // Index 0 = lower case. 
+  // Index 1 = upper case. 
+  // Index 2 = numeric. 
+  // Index 3 = special characters. 
+  const charTypeSelected = [false,false,false,false];
+  const charTypeText = ["lower case", "upper case", "numeric", "special"];
+  let continuing = true;
+
+  while (!validUserInput) {
+    let pwdLen = prompt("Password Length?\n(Between 8 and 128 characters)");
+    if (pwdLen === null) {
+      return "No password generated";
+    } else {
+      // Converts to a number
+      pwdLenNum = +pwdLen;
+      if (isNaN(pwdLenNum)) {
+        responseCode = alert("Must be a number.");
+      } else if (pwdLenNum < 8 || pwdLenNum > 128) {
+        responseCode = alert("Must be at least 8 and at most 128 characters.");
+      } else {
+        validUserInput = true;
+      }
+    }
+  }
+
+  // Index 0 = lower case. 
+  // Index 1 = upper case. 
+  // Index 2 = numeric. 
+  // Index 3 = special characters. 
+  while (!charTypeSelected[0] && !charTypeSelected[1] && !charTypeSelected[2] && !charTypeSelected[3] && continuing) {
+    for (let i = 0; i < 4; i++) {
+      charTypeSelected[i] = confirm("Include " + charTypeText[i] + " characters?\n(OK = Yes, Cancel = No)");
+      // console.log(charTypeSelected[i], charTypeText[i]);
+    }
+
+    if (!charTypeSelected[0] && !charTypeSelected[1] && !charTypeSelected[2] && !charTypeSelected[3]) {
+      continuing = confirm("No character types selected. Do you want to continue?\n(OK = Yes, Cancel = No)");
+      if (!continuing) {
+        return "No password generated";
+      }
+    }
+  }
+  
+
+  return genPassword;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
